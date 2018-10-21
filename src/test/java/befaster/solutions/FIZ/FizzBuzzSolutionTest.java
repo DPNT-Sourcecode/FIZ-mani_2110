c5b096345f5e15c2e0d2e4e73583b1777603910f
sbx_user1060 <sbx_user1060@ip-10-129-129-252.eu-west-2.compute.internal>
2018-10-21 11:40:29 +0000

    Sun Oct 21 11:40:29 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -18,9 +18,27 @@ public class FizzBuzzSolutionTest {
     @Test
     public void compute_hello() {
         assertThat(fizzBuzz.fizzBuzz(1), equalTo("1"));
+        assertThat(fizzBuzz.fizzBuzz(2), equalTo("2"));
+        assertThat(fizzBuzz.fizzBuzz(11), equalTo("11"));
+        assertThat(fizzBuzz.fizzBuzz(31), equalTo("31"));
+        assertThat(fizzBuzz.fizzBuzz(52), equalTo("52"));
+        assertThat(fizzBuzz.fizzBuzz(811), equalTo("811"));
+        assertThat(fizzBuzz.fizzBuzz(997), equalTo("997"));
         assertThat(fizzBuzz.fizzBuzz(3), equalTo("fizz"));
+        assertThat(fizzBuzz.fizzBuzz(9), equalTo("fizz"));
+        assertThat(fizzBuzz.fizzBuzz(51), equalTo("fizz"));
+        assertThat(fizzBuzz.fizzBuzz(111), equalTo("fizz"));
+        assertThat(fizzBuzz.fizzBuzz(369), equalTo("fizz"));
+        assertThat(fizzBuzz.fizzBuzz(3636), equalTo("fizz"));
         assertThat(fizzBuzz.fizzBuzz(5), equalTo("buzz"));
+        assertThat(fizzBuzz.fizzBuzz(10), equalTo("buzz"));
+        assertThat(fizzBuzz.fizzBuzz(100), equalTo("buzz"));
+        assertThat(fizzBuzz.fizzBuzz(500), equalTo("buzz"));
+        assertThat(fizzBuzz.fizzBuzz(1400), equalTo("buzz"));
         assertThat(fizzBuzz.fizzBuzz(15), equalTo("fizz buzz"));
+        assertThat(fizzBuzz.fizzBuzz(30), equalTo("fizz buzz"));
+        assertThat(fizzBuzz.fizzBuzz(105), equalTo("fizz buzz"));
+        assertThat(fizzBuzz.fizzBuzz(1005), equalTo("fizz buzz"));
         assertThat(fizzBuzz.fizzBuzz(101), equalTo("101"));
     }
 }
