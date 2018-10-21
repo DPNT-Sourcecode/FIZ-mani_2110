610dcea0ea5ce120e587c20a833d5bd0a928ea19
sbx_user1060 <sbx_user1060@ip-10-129-129-252.eu-west-2.compute.internal>
2018-10-21 11:25:23 +0000

    Sun Oct 21 11:25:23 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,11 +1,8 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
-//        return x + y;
+        return x + y;
     }
 }
